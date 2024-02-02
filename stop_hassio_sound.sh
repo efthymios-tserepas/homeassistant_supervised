f8ca1be28dfb54ab7ac77d65334c74898861e25b
Efthymios Tserepas <156537892+efthymios-tserepas@users.noreply.github.com>
2024-02-02 19:36:17 +0200

    Add files via upload
    On a Raspberry Pi with an older version of Raspbian 10, if you encounter audio issues after installation, run the following script

---
+++ stop_hassio_sound.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Service file name
+SERVICE_FILE="/etc/systemd/system/stop_hassio_audio.service"
+
+# Service file content
+SERVICE_CONTENT="[Unit]
+Description=Stop hassio_audio container
+After=docker.service
+Requires=docker.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=true
+ExecStart=/bin/bash -c 'sleep 20 && docker stop hassio_audio && docker rm hassio_audio'
+
+[Install]
+WantedBy=multi-user.target
+"
+
+# Write content to the service file
+echo "$SERVICE_CONTENT" | sudo tee "$SERVICE_FILE" > /dev/null
+
+# Update file permissions
+sudo chmod 644 "$SERVICE_FILE"
+
+# Verify that the service file was created successfully
+if [ -e "$SERVICE_FILE" ]; then
+    echo "The service file was created successfully: $SERVICE_FILE"
+else
+    echo "There was an issue creating the service file."
+fi
+
